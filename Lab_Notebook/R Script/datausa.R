93d80f5cf95d9fdb859349bc61896883f34f7778
Gina <92118843+ginazha@users.noreply.github.com>
2026-02-10 18:53:37 -0800

    Add files via upload

---
+++ Lab_Notebook/R Script/datausa.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+
+
+#Load the metadata in
+metadata <- read.table("metadata.tsv", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+
+#Remove Edinburgh, Buenos Aires, and San Sebastian
+str(metadata)
+colnames(metadata)
+unique(metadata$site_x)
+table(metadata$site_x)
+
+remove_site <- c("Edinburgh", "Buenos Aires", "San Sebastian")
+metadata_usa <- metadata[ !metadata$site_x %in% remove_site, ]
+
+unique(metadata_usa$site_x)
+
+#Check the different types of MS, see the counts for each group
+
+unique(metadata_usa$disease_course)
+
+disease_type <- (metadata_usa %>% count(disease_course))
+print(disease_type)
+
+disease_sex <- table(metadata_usa$disease_course, metadata_usa$sex)
+print(disease_sex)
+
+#Export edited metadata tsv file
+
+library(readr)
+
+write_tsv(metadata_usa, "metadata_usa.tsv")
+
+#Edit the manifest data to match
+keep_samples <- metadata_usa$sample.id
+
+manifest <- read.table("manifest.tsv", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+
+manifest_usa <- manifest[manifest$`sample.id` %in% keep_samples, ]
+
+write_tsv(manifest_usa, "manifest_usa.tsv")
+
+#Check if the manifest and the metadata match :)
+metadata_ids <- metadata_usa$sample.id
+manifest_ids <- manifest_usa$sample.id
+
+all(manifest_ids %in% metadata_ids)
